327c09e32a838712b36268409db54189c53ef6aa
mgrenson <mgrenson@BRUL-CHQGFC2.contraste.loc>
2017-03-15 15:36:28 +0100

    Hériter les modèles Governement et Minister de TrashableModel

--- app/models/Governement.php
+++ app/models/Governement.php
@@ -2,27 +2,18 @@
 /**
  * Gouvernements
  *
- * @property int            $id          (PK)
  * @property string         $name        Maximum 255 caractères
  * @property string         $shortname   Maximum 255 caractères
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- * @property \Carbon\Carbon $deleted_at
  *
  * @author jdavreux
  */
-class Governement extends Eloquent {
+class Governement extends TrashableModel {
 	
-	use SoftDeletingTrait;
 	protected $table = 'governements';
-	// public $arrayAdministrations = array();
 	
-	/*
-	 * public static function getGovernementsByAlphabeticalOrder( ) {
-	 *
-	 * return Governement::orderBy('name')->get();
-	 *
-	 * }
+	/**
+	 * 
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
 	 */
 	public function ministers() {
 		return $this->belongsToMany ( 'Minister' );
--- app/models/Minister.php
+++ app/models/Minister.php
@@ -2,34 +2,28 @@
 /**
  * Ministres
  *
- * @property int            $id              (PK)
  * @property string         $firstname       Maximum 255 caractères
  * @property string         $lastname        Maximum 255 caractères
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- * @property \Carbon\Carbon $deleted_at
  *
  * @author jdavreux
  */
-class Minister extends Eloquent {
+class Minister extends TrashableModel {
 	
-	use SoftDeletingTrait;
 	protected $table = 'ministers';
+	
+	/**
+	 * 
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
 	public function ideas() {
 		return $this->belongsToMany ( 'Idea' );
 	}
+	
+	/**
+	 * 
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
 	public function governements() {
 		return $this->belongsToMany ( 'Governement' );
-	}
-	
-	/*
-	 * public static function getAMinistersByGovernementId( $governementId ) {
-	 *
-	 * return Minister::where('regions_id', '=', $regionId)->get();
-	 *
-	 * }
-	 */
-	public static function getArrayOfGovernementsAndMinisters() {
-		return Governement::getGovernementsByAlphabeticalOrder ();
 	}
 }
