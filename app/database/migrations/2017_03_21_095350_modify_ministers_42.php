87123e825538011ce46645fa9beef094d954284b
mgrenson <mgrenson@BRUL-CHQGFC2.contraste.loc>
2017-05-09 13:27:14 +0200

    Code commenté inutile

--- app/database/migrations/2017_03_21_095350_modify_ministers_42.php
+++ app/database/migrations/2017_03_21_095350_modify_ministers_42.php
@@ -18,15 +18,12 @@ class ModifyMinisters42 extends Migration {
 	 * @return void
 	 */
 	public function up() {
-		//$output=new ConsoleOutput();
-		//DB::statement('CREATE EXTENSION btree_gist;');
 		
 		DB::statement('ALTER TABLE governement_minister ADD COLUMN mandate_range daterange NOT NULL DEFAULT(\'[2014-01-01,2017-12-31]\'), ADD EXCLUDE USING gist (minister_id WITH =, governement_id WITH =, mandate_range WITH &&);');
 		
 		Schema::table( 'governement_minister', function (Blueprint $table) {
 			$table->dropColumn(['start', 'end']);
 			$table->increments ( 'id' )->unsigned ();
-			//$table->primary(['minister_id', 'governement_id', 'mandate_range'], 'governement_minister_pkey');
 		});
 		
 		DB::statement('ALTER TABLE "ministers" ALTER COLUMN "firstname" SET NOT NULL;');
@@ -43,15 +40,11 @@ class ModifyMinisters42 extends Migration {
 		DB::statement('ALTER TABLE "ministers" ALTER COLUMN "firstname" DROP NOT NULL;');
 		DB::statement('ALTER TABLE "ministers" ALTER COLUMN "lastname" DROP NOT NULL;');
 		
-		//DB::statement('ALTER TABLE governement_minister DROP CONSTRAINT governement_minister_pkey');
-		
 		Schema::table( 'governement_minister', function (Blueprint $table) {
 			$table->timestamp ( 'start' )->default('2014-01-01 00:00:00');
 			$table->timestamp ( 'end' )->default('2017-12-31 23:59:59');
 			$table->dropColumn('mandate_range');
 			$table->dropColumn('id');
 		});
-		
-		//DB::statement('DROP EXTENSION btree_gist;');
 	}
 }
