97d7b083caf217f9149d0358d8e59bee72342ed8
mgrenson <mgrenson@BRUL-CHQGFC2.contraste.loc>
2017-06-16 17:11:33 +0200

    Aadaptation des tests pour l'usage synapse

--- app/tests/models/PermissionTest.php
+++ app/tests/models/PermissionTest.php
@@ -1,9 +1,9 @@
 <?php
 use Woodling\Woodling;
 class PermissionTest extends TestCase {
+	
 	public function testName() {
 		$permission = Woodling::retrieve ( 'manage_posts' );
-		
 		$this->assertEquals ( $permission->name, 'manage_posts' );
 		$this->assertEquals ( $permission->display_name, 'manage posts' );
 	}
